814cce3c770918f666b1b053ab8249a65cf33568
Cryoxe <arthmal@icloud.com>
2022-11-01 16:35:01 +0100

    Rework and bug correction

--- SimplyCard/Cards/PortraitOfMarkov.cs
+++ SimplyCard/Cards/PortraitOfMarkov.cs
@@ -9,7 +9,7 @@ using UnboundLib.Cards;
 using ModdingUtils.Extensions;
 using UnityEngine;
 
-namespace SimplyCard.Cards
+namespace ExtraGameCards.Cards
 {
     class PortraitOfMarkov : CustomCard
     {
@@ -29,10 +29,9 @@ namespace SimplyCard.Cards
             gun.spread *= 0.7f;
             characterStats.lifeSteal = (characterStats.lifeSteal != 0f) ? (characterStats.lifeSteal * 2) : (characterStats.lifeSteal + 1f);
 
-            Unbound.Instance.StartCoroutine(ExtraPicks(player));
-
-            //GameModeManager.AddOnceHook(GameModeHooks.HookPlayerPickStart, (gm) => { EGC.Instance.StartCoroutine(MarkovPick(player)); return new List<object>().GetEnumerator(); ; });
+            GameModeManager.AddOnceHook(GameModeHooks.HookPlayerPickEnd, (gm) => { EGC.Instance.StartCoroutine(TreasurePick(player)); return new List<object>().GetEnumerator();});
         }
+        
         public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
         {
             //UnityEngine.Debug.Log($"[{ExtraCards.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
@@ -100,27 +99,30 @@ namespace SimplyCard.Cards
             return EGC.ModInitials;
         }
 
-        internal static IEnumerator ExtraPicks(Player player)
+        public static IEnumerator TreasurePick(Player player)
         {
-            yield return GameModeManager.TriggerHook(GameModeHooks.HookPlayerPickStart);
-
-            player.data.stats.GetAdditionalData().blacklistedCategories.Add(EGC.Normal);
-            player.data.stats.GetAdditionalData().blacklistedCategories.Add(EGC.Lunar);
-            player.data.stats.GetAdditionalData().blacklistedCategories.Remove(EGC.Markov);
-            yield return new WaitForSecondsRealtime(0.1f);
-
-            CardChoiceVisuals.instance.Show(Enumerable.Range(0, PlayerManager.instance.players.Count).Where(i => PlayerManager.instance.players[i].playerID == player.playerID).First(), true);
-            yield return CardChoice.instance.DoPick(1, player.playerID, PickerType.Player);
-            yield return new WaitForSecondsRealtime(0.1f);
-
-            yield return GameModeManager.TriggerHook(GameModeHooks.HookPlayerPickEnd);
-
-            player.data.stats.GetAdditionalData().blacklistedCategories.Add(EGC.Markov);
-            player.data.stats.GetAdditionalData().blacklistedCategories.Remove(EGC.Normal);
-            player.data.stats.GetAdditionalData().blacklistedCategories.Remove(EGC.Lunar);
-            yield return new WaitForSecondsRealtime(0.1f);
+            while (!CardChoice.instance.IsPicking) yield return null;
+            if (CardChoice.instance.pickrID == player.playerID)
+            {
+                player.data.stats.GetAdditionalData().blacklistedCategories.Add(EGC.Normal);
+                player.data.stats.GetAdditionalData().blacklistedCategories.Add(EGC.Lunar);
+                player.data.stats.GetAdditionalData().blacklistedCategories.Remove(EGC.Markov);
+                GameModeManager.AddOnceHook(GameModeHooks.HookPlayerPickEnd, (gm) => EndTreasurePick(player));
+            }
+            else
+                GameModeManager.AddOnceHook(GameModeHooks.HookPlayerPickStart, (gm) => { EGC.Instance.StartCoroutine(TreasurePick(player)); return new List<object>().GetEnumerator(); });
             yield break;
         }
 
+        public static IEnumerator EndTreasurePick(Player player)
+        {
+            player.data.stats.GetAdditionalData().blacklistedCategories.Add(EGC.Markov);
+            player.data.stats.GetAdditionalData().blacklistedCategories.Remove(EGC.Normal);
+            player.data.stats.GetAdditionalData().blacklistedCategories.Remove(EGC.Lunar);
+            //CardChoiceVisuals.instance.Show(Enumerable.Range(0, PlayerManager.instance.players.Count).Where(i => PlayerManager.instance.players[i].playerID == player.playerID).First(), true);
+            //yield return CardChoice.instance.DoPick(1, player.playerID, PickerType.Player);
+            yield break;
+        }
     }
+
 }
